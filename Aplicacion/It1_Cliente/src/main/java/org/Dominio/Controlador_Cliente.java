b3bf513071d8bb88d6824de889d50d296b764e69
AlonsoVM <Alonso.Villamayor@alu.uclm.es>
2022-11-14 14:57:34 +0100

    Implementación de la parte cliente CDU1

---
+++ Aplicacion/It1_Cliente/src/main/java/org/Dominio/Controlador_Cliente.java
@@ -0,0 +1,15 @@
+package org.Dominio;
+
+public class Controlador_Cliente {
+
+	/**
+	 * 
+	 * @param Username
+	 * @param Password
+	 */
+	public void Comprobar_Credenciales(String Username, String Password) {
+		// TODO - implement Controlador_Cliente.Comprobar_Credenciales
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Aplicacion/It1_Cliente/src/main/java/org/Presentacion/IU_ClienteLogin.java
@@ -0,0 +1,15 @@
+package org.Presentacion;
+
+public class IU_ClienteLogin {
+
+	/**
+	 * 
+	 * @param Username
+	 * @param Password
+	 */
+	public void Comprobar_Credenciales(String Username, String Password) {
+		// TODO - implement IU_ClienteLogin.Comprobar_Credenciales
+		throw new UnsupportedOperationException();
+	}
+
+}
