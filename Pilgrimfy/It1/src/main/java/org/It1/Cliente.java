6f6310b0185a1131a100a66066aaaa1e292cfecd
AlonsoVM <Alonso.Villamayor@alu.uclm.es>
2022-11-16 19:02:04 +0100

    Implementación iteración 1
    Se ha creado un modulo maven en la dentro del proyecto maven principal Pilgrimfy

---
+++ Pilgrimfy/It1/src/main/java/org/It1/Cliente.java
@@ -0,0 +1,29 @@
+package org.It1;
+
+public class Cliente {
+
+	private String Usuario;
+	private String password;
+	private String Tipo_Usuario;
+	private DAO_Cliente DAO_Obj;
+
+	public String getUsuario() {
+		// TODO - implement Cliente.getUsuario
+		throw new UnsupportedOperationException();
+	}
+
+	public String getpassword() {
+		return this.password;
+	}
+
+	public String Tipo_Usuario() {
+		// TODO - implement Cliente.Tipo_Usuario
+		throw new UnsupportedOperationException();
+	}
+
+	public void Comprobar_Credenciales() {
+		// TODO - implement Cliente.Comprobar_Credenciales
+		throw new UnsupportedOperationException();
+	}
+
+}
