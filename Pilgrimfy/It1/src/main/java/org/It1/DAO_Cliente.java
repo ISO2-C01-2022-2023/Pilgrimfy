cd31796aa2ba5cd32cb531a2602458d56d963db7
AlonsoVM <Alonso.Villamayor@alu.uclm.es>
2022-11-17 17:09:07 +0100

    Correción It1 v 0.1.0
    El commit anterior contenía un error por la configuración del eclipse, ya esta solucionado

---
+++ Pilgrimfy/It1/src/main/java/org/It1/DAO_Cliente.java
@@ -0,0 +1,14 @@
+package org.It1;
+
+public class DAO_Cliente {
+
+	/**
+	 * 
+	 * @param Consulta
+	 */
+	public void Comprobar_Credenciales(String Consulta) {
+		// TODO - implement DAO_Cliente.Comprobar_Credenciales
+		throw new UnsupportedOperationException();
+	}
+
+}
