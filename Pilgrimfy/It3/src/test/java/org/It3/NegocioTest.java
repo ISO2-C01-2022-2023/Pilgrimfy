b611c373b9c3fb4cd8c4036bc466df23e97a9c4d
AlonsoVM <Alonso.Villamayor@alu.uclm.es>
2022-12-06 19:37:53 +0100

    Pruebas CD3
    Fin de las pruebas CDU3

---
+++ Pilgrimfy/It3/src/test/java/org/It3/NegocioTest.java
@@ -0,0 +1,8 @@
+package org.It3;
+
+public class NegocioTest {
+    public void TestNegocio()
+    {
+        assertTrue( true );
+    }
+}
---
+++ Pilgrimfy/It3/src/test/java/org/It3/controlTest.java
@@ -0,0 +1,34 @@
+package org.It3;
+
+public class controlTest {
+    @Test
+    public void control_AñadirNuevoNegocio()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void control_BorrarPerfilNegocio()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void control_EditarInfoNegocio()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void control_MostrarPlazas()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void control_MostrarGanancias()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void control_MostrarPeregrinos()
+    {
+        assertTrue( true );
+    }
+}
