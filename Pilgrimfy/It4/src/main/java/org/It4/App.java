c7715880ef43ab44da9e1c1283c711a0ad7ef392
jorge-ESI <72616700+jorge-ESI@users.noreply.github.com>
2022-12-09 19:04:36 +0100

    añado módulo 4

---
+++ Pilgrimfy/It4/src/main/java/org/It4/App.java
@@ -0,0 +1,13 @@
+package org.It4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
